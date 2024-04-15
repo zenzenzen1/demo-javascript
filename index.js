4b80c6543a6897b9c96d274d369632984437949e
N X T <truongnxhe176705@fpt.edu.vn>
2024-04-16 00:06:40 +0700

    Consolog, operator, operand, datatype demo

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log("Hello world from index.js");
+
+let x;
+x = "abc";
+x = 1;
+x = `a
+aaa '"\``;
+var y = "ab \'\"\\`" +
+"c";
+var c;
+var d = {};
+x = 1;
+console.log(x + "\n" + y + " " + typeof y + " " + typeof c);
+console.log("typeof d", typeof d);
+console.log("2", + "x = " + x + "\ny = " + y + "\nc = " + c);
+console.log("", + x );
+console.log({a: "abc"} + "abc");
+
+x = "abcd";
+console.log(typeof x === "string");
+
