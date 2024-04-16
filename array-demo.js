b3ddc64906cca267ddaf1e2e7f83a32897c25acb
N X T <truongnxhe176705@fpt.edu.vn>
2024-04-16 14:56:36 +0700

    array-demo

---
+++ array-demo.js
@@ -0,0 +1,12 @@
+console.log("array-demo");
+let arr = ["a", "b", "c"];
+
+console.log(arr.push("d"));
+arr.unshift(1);
+
+console.log(arr);
+
+// array of objects
+arr = [{name: "nxt", age: 18}, {name: "ThanhMv", age: 20}];
+console.log(arr);
+
