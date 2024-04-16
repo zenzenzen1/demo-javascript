7bfd1efa517629c09ff0d8af5d8f7705fbaf7857
N X T <truongnxhe176705@fpt.edu.vn>
2024-04-16 18:08:50 +0700

    Empty, undefined, null

--- index.js
+++ index.js
@@ -7,15 +7,20 @@ x = `a
 aaa '"\``;
 var y = "ab \'\"\\`" +
 "c";
-var c;
 var d = {};
 x = 1;
-console.log(x + "\n" + y + " " + typeof y + " " + typeof c);
+console.log(x + "\n" + y + " " + typeof y + " ");
 console.log("typeof d", typeof d);
-console.log("2", + "x = " + x + "\ny = " + y + "\nc = " + c);
+console.log("2", + "x = " + x + "\ny = " + y);
 console.log("", + x );
 console.log({a: "abc"} + "abc");
 
 x = "abcd";
 console.log(typeof x === "string");
 
+// Empty, undefined, null
+let a, b, c;
+a = {};
+console.log(a, b, c);
+console.log(isNaN(a));
+console.log()
