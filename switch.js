8c758704d3e7e22d7c89e5a9cfc9947bf9880812
N X T <truongnxhe176705@fpt.edu.vn>
2024-04-17 22:57:21 +0700

    witch demo

---
+++ switch.js
@@ -0,0 +1,25 @@
+let key = "b";
+
+switch (key) {
+    case "aa":
+    case "cc":
+    case "bb": {
+        
+    }
+    default:
+        console.log("default");
+    case "a":
+        console.log("a");
+        break;
+    case "b":{
+        console.log("b");
+    }
+}
+
+console.log("end ");
+
+var v = {
+    name: "abcd",
+    
+}
+console.log(v.name);
