e9848ab39b7f1b40ed8461262687cb5a54572393
N X T <truongnxhe176705@fpt.edu.vn>
2024-04-18 18:49:08 +0700

    filter demo

--- array-demo.js
+++ array-demo.js
@@ -10,3 +10,26 @@ console.log(arr);
 arr = [{name: "nxt", age: 18}, {name: "ThanhMv", age: 20}];
 console.log(arr);
 
+arr = [2, 5, 2, 6, 2, 1, {name: "Nxt", age: 19}];
+let filter = arr.filter((item, index, a) => {
+    !isNaN(item);
+    return item instanceof Object;
+});
+console.log(filter);
+
+arr = [
+    {name: "Nxt", age: 22},
+    {name: "abc", age: 29},
+    {name: "ThanhMV", age: 20},
+    {name: "zz", age: 5}
+];
+
+filter = arr.filter((value, index) => {
+    return value.age >= 22;
+});
+console.log(filter);
+
+console.log(arr.find((t) => {
+    console.log(t.name);
+    return (t.name + "").startsWith("N");
+}));
